193a33728149eb622a25408ac4bafd4bc7f8ff5e
Sam-PC\Sam <samlaw2198@gmail.com>
2020-04-22 15:53:39 -0500

    Commiting backlog of previous updates

--- client/src/client.js
+++ client/src/client.js
@@ -1,16 +1,18 @@
-//Testing early prototypes of black and white cards.
-import BlackCard from '/src/blackcard.js';
-
-var my_black_card = new BlackCard('Sam wants', 'every day.');
-
 //Player's current deck.
 var my_cards = [];
 
 //Cards that are available to the player once they use one in their deck.
 var available_cards = [];
 
-//List of names of all players currently on server.
+//List of names of all players currently on server and their scores.
 var names = [];
+var scores = [];
+var current_prompt = 'I go to _____ class.';
+
+var canvas = document.querySelector('#card-screen');
+canvas.width = canvas.parentNode.clientWidth;
+canvas.height = canvas.parentNode.clientHeight;
+var c = canvas.getContext('2d');
 
 //Create new <li> element in #chat-history to represent chat with given text.
 const writeChat = (text) => {
@@ -26,7 +28,10 @@ const onChatSubmitted = (event) => {
 	const input = document.querySelector('#chat');
 	const text = input.value;
 	input.value = '';
-	if (socket.name == null) {
+	if (socket.room == null) {
+		socket.emit('room', text);
+	}
+	else if (socket.name == null) {
 		socket.emit('name_set', text);
 	}
 	else {
@@ -59,8 +64,19 @@ const onCardSubmitted = (event) => {
 	if (socket.name != null && my_cards[i] != null) socket.emit('action', my_cards[i]);
 }
 
+const createBlackCard = (text) => {
+	c.fillRect(canvas.width/2-canvas.width/10, canvas.height/2-canvas.height*.4, canvas.width/5, canvas.height/2);
+	c.font = (canvas.width/100 * 1.5) + 'px Arial';
+	c.fillStyle = 'white';
+	c.textAlign = 'center';
+	c.fillText(text, canvas.width/2, canvas.height/2-100)
+	c.font = (canvas.width/100) + 'px Arial';
+	c.fillText('Cards Against the Internet', canvas.width/2-canvas.width/30, canvas.height/2+canvas.height/12);
+}
+
 //When receiving a message from the server, do writeChat function.
-const socket = io();
+const socket = io.connect();
+
 socket.on('message', (text) => {
 	writeChat(text);
 });
@@ -68,6 +84,12 @@ socket.on('message', (text) => {
 //Set the player's name locally if it is allowed by the server.
 socket.on('name_set', (name) => {
 	socket.name = name;
+});
+
+socket.on('room_set', (room) => {
+	socket.room = room;
+	const title = document.querySelector('#leaderboard-title');
+	title.textContent = 'Players - Room: ' + room;
 });
 
 //Set the player's deck when they first join.
@@ -82,25 +104,33 @@ socket.on('create_deck', (available_cards) => {
 	document.querySelector('#c3').textContent = my_cards[2];
 	document.querySelector('#c4').textContent = my_cards[3];
 	document.querySelector('#c5').textContent = my_cards[4];
+	document.querySelector('#c1').style.visibility = 'visible';
+	document.querySelector('#c2').style.visibility = 'visible';
+	document.querySelector('#c3').style.visibility = 'visible';
+	document.querySelector('#c4').style.visibility = 'visible';
+	document.querySelector('#c5').style.visibility = 'visible';
+	
+	createBlackCard(current_prompt);
 });
 
 //Update the list of player names when the player list is updated.
-socket.on('player_update', (new_names) => {
-	names = new_names;
+socket.on('player_update', (clients) => {
+	console.log(clients);
 	const parent = document.querySelector('#leaderboard');
 	while (parent.firstChild) {
 		parent.removeChild(parent.firstChild);
 	}
-	for (let i = 0; i < names.length; i++) {
+	for (let i = 0; i < clients.length; i++) {
+		console.log(clients[i]);
 		const new_li = document.createElement('li');
-		new_li.textContent = names[i];
+		new_li.textContent = clients[i];
 		parent.appendChild(new_li);
 	}
 });
 
 //Default message upon first joining.
 writeChat('SYSTEM: Hello, welcome to Cards Against the Internet!');
-writeChat('SYSTEM: Please enter your name in chat before starting.')
+writeChat('SYSTEM: Please enter a 4-character room code. You may join an existing room or create your own.')
 
 //Event Listeners.
 document.querySelector('#chat-form').addEventListener('submit', onChatSubmitted);
@@ -110,3 +140,11 @@ document.querySelector('#c2').addEventListener('click', onCardSubmitted);
 document.querySelector('#c3').addEventListener('click', onCardSubmitted);
 document.querySelector('#c4').addEventListener('click', onCardSubmitted);
 document.querySelector('#c5').addEventListener('click', onCardSubmitted);
+
+//Resive Canvas when window changes size
+window.addEventListener("resize", resizeCanvas, false);
+function resizeCanvas(e) {
+	canvas.width = canvas.parentNode.clientWidth;
+	canvas.height = canvas.parentNode.clientHeight;
+	if (socket.name != null) createBlackCard(current_prompt);
+}
--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ console.log(`Serving static from ${client_dir}`);
 app.use(express.static(client_dir));
 
 const server = http.createServer(app);
-const io = socketio(server);
-
-//Clients is a list of all clients, names is a list of all currently taken names.
-var clients = [];
-var names = [];
+const io = socketio.listen(server);
 
 //Master list of cards.
 var all_white_cards = [];
@@ -22,50 +18,72 @@ all_white_cards[1] = 'Apples to Apples';
 all_white_cards[2] = 'Computer Science';
 all_white_cards[3] = 'Cold winter weather in Iowa';
 all_white_cards[4] = 'Socket.IO';
-all_white_cards[5] = 'Discord'
-all_white_cards[6] = 'Capstone'
-all_white_cards[7] = 'Simpson College'
-all_white_cards[8] = 'Carver Science Lab'
-all_white_cards[9] = 'Pfeiffer'
-all_white_cards[10] = 'MICS'
-all_white_cards[11] = 'CATI'
-all_white_cards[12] = 'Hello World!'
-all_white_cards[13] = 'white_cards[13]'
-all_white_cards[14] = 'Don\'t Click This'
+all_white_cards[5] = 'Discord';
+all_white_cards[6] = 'Capstone';
+all_white_cards[7] = 'Simpson College';
+all_white_cards[8] = 'Carver Science Lab';
+all_white_cards[9] = 'Pfeiffer';
+all_white_cards[10] = 'MICS';
+all_white_cards[11] = 'CATI';
+all_white_cards[12] = 'Hello World!';
+all_white_cards[13] = 'white_cards[13]';
+all_white_cards[14] = 'Don\'t Click This';
 all_white_cards[15] = 'Beep Beep Boop';
 
 //Currently available cards in the current session. Starts off as full as master list.
 var available_cards = all_white_cards.slice();
 
+function player_update(room) {
+	//Don't do anything if the last person leaves, and the room becomes empty
+	if (io.sockets.adapter.rooms[room] != null) {
+		//Get list of all clients in room and send their names and scores to clients.
+		var clients = io.sockets.adapter.rooms[room].sockets;
+		var names_scores = [];
+		for (var clientID in clients) {
+			var clientSocket = io.sockets.connected[clientID];
+			names_scores.push(clientSocket.name + ' - Score: ' + clientSocket.score);
+		}
+		io.sockets.in(room).emit('player_update', names_scores);
+	}
+}
+
 //Receiving connection from clients (socket.emit for local, io.emit for server)
-io.on('connection', (socket) => {
+io.sockets.on('connection', (socket) => {
 	//Add client to the list of clients, and let the server know a player is in.
-	clients.push(socket);
 	console.log('A player has connected');
-	socket.emit('player_update', names);
 	
-	//When client sends a message to the server, send the text to all clients.
-	socket.on('message', (text) => {
-		io.emit('message', socket.name + ': ' + text);
-		//debug
-		if (text == '!resubmit') socket.played = false;
-		if (text == '!refresh') available_cards = all_white_cards.slice();
-	});
-	
-	//If the client hasn't played yet, when client plays a card, send the contents of the card.
-	socket.on('action', (content) => {
-		if (!socket.played) {
-			io.emit('message', 'SYSTEM: ' + socket.name + ' played the "' + content + '" card.');
-			socket.played = true;
+	//Have the player join a specified room. If they are the creator of the room, they will start as the judge and be granted special permissions.
+	socket.on('room', (room) => {
+		room = room.toUpperCase();
+		if (room.length != 4 || room.includes(' ') || room.includes('	')) {
+			socket.emit('message', 'SYSTEM: Please enter a valid room code.');
+		}
+		else {
+			socket.join(room);
+			socket.room = room;
+			socket.emit('room_set', room);
+			if (io.sockets.adapter.rooms[room].length == 1) {
+				socket.host = true;
+				socket.judge = true;
+				socket.emit('message', 'SYSTEM: You are the host of the "' + room + '" room! Enter your name, and type "!start" to begin the game.');
+				io.sockets.adapter.rooms[room].available_cards = all_white_cards.slice();
+			}
+			else socket.emit('message', 'SYSTEM: You joined the "' + room + '" room. Now, please enter your name before starting.');
 		}
 	});
 	
 	//When client sets a name, check if it's available. If it is, assign them the name. Otherwise, have them do it again.
 	socket.on('name_set', (name) => {
+		var clients = io.sockets.adapter.rooms[socket.room].sockets;
+		
+		//Don't allow name if it's already in the room.
 		var duplicate = false;
-		for(let i = 0; i < names.length; i++) {
-			if(names[i].toUpperCase() == name.toUpperCase()) {
-				duplicate = true;
+		for (var clientID in clients) {
+			var clientSocket = io.sockets.connected[clientID];
+			if (clientSocket.name != null) {
+				if (name.toUpperCase() == clientSocket.name.toUpperCase()) {
+					duplicate = true;
+				}
 			}
 		}
 		if (duplicate || name.toUpperCase() == 'SYSTEM' || name == '') {
@@ -77,34 +95,53 @@ io.on('connection', (socket) => {
 		else if (name.length >= 15) {
 			socket.emit('message', 'SYSTEM: Sorry, names are limited to 15 characters or less. Try again.');
 		}
+		
 		else {
-			//Give player their name, add name to list of names, and send list of names to client.
-			names.push(name);
+			//Give player their name, update the client's list of player names, and create their deck.
 			socket.name = name;
+			socket.score = 0;
 			socket.emit('name_set', name);
 			socket.played = false;
-			io.emit('message', 'SYSTEM: ' + socket.name + ' has connected to the server.');
-			io.emit('player_update', names);
+			io.sockets.in(socket.room).emit('message', 'SYSTEM: ' + socket.name + ' has joined the room!');
+			player_update(socket.room);
 			
-			//Randomly generate deck of cards for player.
+			//Randomly generate deck of cards for player from the list of available cards in the room.
 			var player_deck = [];
 			for(let i = 0; i < 5; i++) {
-				var rand = Math.floor(Math.random()*available_cards.length);
-				player_deck[i] = available_cards[rand];
-				available_cards.splice(rand,1);
+				var rand = Math.floor(Math.random() * io.sockets.adapter.rooms[socket.room].available_cards.length);
+				player_deck[i] = io.sockets.adapter.rooms[socket.room].available_cards[rand];
+				io.sockets.adapter.rooms[socket.room].available_cards.splice(rand,1);
 			}
 			socket.emit('create_deck', player_deck);
 		}
 	});
 	
-	//Let the server know when a player is leaving. Remove the client and their name from the respective lists.
+	//When client sends a message to the server, send the text to all clients.
+	socket.on('message', (text) => {
+		io.sockets.in(socket.room).emit('message', socket.name + ': ' + text);
+		//debug
+		if (text == '!resubmit') socket.played = false;
+		if (text == '!refresh') io.sockets.adapter.rooms[socket.room].available_cards = all_white_cards.slice();
+		if (text == '!giveScore') {
+			socket.score += 1;
+			player_update(socket.room);
+		}
+	});
+	
+	//If the client hasn't played yet, when client plays a card, send the contents of the card.
+	socket.on('action', (content) => {
+		if (!socket.played) {
+			io.sockets.in(socket.room).emit('message', 'SYSTEM: ' + socket.name + ' played the "' + content + '" card.');
+			socket.played = true;
+		}
+	});
+	
+	//Let the room know when a player is leaving. Remove the client from the room list.
 	socket.on('disconnect', function() {
 		console.log('A player has disconnected.');
-		clients.splice(clients.indexOf(socket),1);
 		if (socket.name != null) {
-			io.emit('message', 'SYSTEM: ' + socket.name + ' has left the server.');
-			names.splice(names.indexOf(socket.name),1);
-			io.emit('player_update', names);
+			io.sockets.in(socket.room).emit('message', 'SYSTEM: ' + socket.name + ' has left the room.');
+			player_update(socket.room);
 		}
 	});
 });
